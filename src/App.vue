66b6deeb9da29ffea1aac47c260ce61507b18546
sintof <dcpersonf@gmail.com>
2024-02-01 23:30:52 +0500

    Fixed problems with adaptibility ( turned off resizing for large screens)

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import FooterNav from './components/FooterNav.vue';
 </script>
 
 <template>
-<div class="h-screen flex justify-between flex-col bg-secondary font-inter">
+<div class="h-screen flex justify-between flex-col bg-secondary font-inter max-w-[30rem] mx-auto">
     <Logo />
     <RouterView />
-    <FooterNav class="fixed bottom-0" />
-  </div>
+    <FooterNav class="fixed bottom-0 max-w-[30rem]" />
+</div>
 </template>
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="flex gap-2 items-center cursor-pointer p-2 bg-white rounded-xl">
-        <img src="../assets/pp.png" alt="pp" class="w-10 h-10">
-        <div class="text-black flex flex-col items-start">
-            <div class="text-sm">Math Teacher</div>
-            <div class="text-sm font-sans line-clamp-1 text-left">Tomorrow bring your laptops for practice</div>
+    <div class="flex gap-2 justify-between items-center cursor-pointer p-2 bg-white rounded-xl">
+        <div class="flex gap-4">
+            <img src="../assets/pp.png" alt="pp" class="w-10 h-10">
+            <div class="text-black flex flex-col items-start">
+                <div class="text-sm">Math Teacher</div>
+                <div class="text-sm font-sans line-clamp-1 text-left">Tomorrow bring your laptops for practice</div>
+            </div>
         </div>
-        <div class="flex flex-col justify-end text-xs font text-black">10:00</div>
-        <EllipsisIcon class="rotate-90 w-8" />
+        <div class="flex gap-4">
+            <div class="flex flex-col justify-end text-xs font text-black">10:00</div>
+            <EllipsisIcon class="rotate-90 w-8" />
+        </div>
     </div>
 </template>
 
